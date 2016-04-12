6b5ab1da9c4d318c126967946d71e92fe2d997b3
Brian Wagner <briwagner@outlook.com>
2016-04-11 21:35:00 -0400

    Refactor last_page_read method to return 0 if fails; edit views to fix errors on incomplete data

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,9 +3,17 @@ class Book < ActiveRecord::Base
   belongs_to :user
   has_many :readings
 
+  validates :pagecount, presence: true
+
   def last_page_read
-    last_reading = self.readings.max { |a, b| a.endpage <=> b.endpage }
-    last_reading.endpage
+    if self.readings.count > 2
+      last_reading = self.readings.max { |a, b| a.endpage <=> b.endpage }
+      last_reading.endpage
+    elsif self.readings.count == 1 && self.readings.last.endpage
+      self.readings.last.endpage
+    else
+      0
+    end
   end
 
   def current_reading
